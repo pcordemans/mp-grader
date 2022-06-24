f6c7236e28daf95e17402630f2fe32b7eb47ad22
Piet Cordemans <cordemans.piet@gmail.com>
2022-06-24 13:59:24 +0200

    adds the interactive shell

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,23 @@
 import scala.language.reflectiveCalls
 import scala.io.StdIn.readLine
+import scala.collection.mutable.Map
 
 @main def mpGrader: Unit = {
     banner()
-    val path = getPath()
-    Console.println(s"Reading: $path")
-    //readTextFile(path)
+    val answerKey = getFile("Answer Key")
+    printNumberOfAnswers(answerKey)
+    val studentAnswers = getFile("Student Answers")
+    printNumberOfAnswers(studentAnswers)
+    readLine("Continue? (y)") match {
+        case "n" =>
+        case _ => 
+            val grader = Grader(answerKey)
+            grader.grade(studentAnswers) match {
+                case Some(grade) => Console.println(grade)
+                case None => Console.println("Well, this is awkward...")
+            }
+    }
+
 }
 
 object Control {
@@ -44,4 +56,21 @@ def getPath():String={
         case "" => "./multiple-choice.md"
         case path => path
     }
+}
+
+def getFile(nameOfFile: String): Map[Int, Answer] = {
+    Console.println(s"What is the path of the $nameOfFile?")
+    val path = getPath()
+    Console.println(s"Reading: $path")
+    readTextFile(path) match {
+        case Some(file) => Parser.parse(file)
+        case None => 
+            Console.println(s"$nameOfFile not found at $path") 
+            getFile(nameOfFile)
+    } 
+}
+
+def printNumberOfAnswers(parsedQuestions: Map[Int, Answer]) = {
+    val numberOfQuestions = parsedQuestions.size
+    Console.println(s"Found $numberOfQuestions")
 }
